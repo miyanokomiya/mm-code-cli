e72f858ee4510fd0fab9abd4220bad50744e55b5
miyanokomiya <komiyamatomoya@mbp.local>
2017-11-16 22:38:03 +0900

    ビルドファイル更新

--- out/extension.js
+++ out/extension.js
@@ -21,8 +21,8 @@ function activate(context) {
             serverProcess.kill();
             serverProcess = null;
         }
-        // const binPath = '~/.vscode/extensions/mm-code/mm-code'
-        const binPath = '/Users/komiyamatomoya/develop/vscode/mm-code/mm-code/mm-code';
+        const binPath = '~/.vscode/extensions/mm-code/mm-code';
+        // const binPath = '/Users/komiyamatomoya/develop/vscode/mm-code/mm-code/mm-code'
         serverProcess = child_process.exec(binPath, (error, stdout, stderror) => {
             // if (error) {
             //     vscode.window.showErrorMessage('MM failed to start server.')
